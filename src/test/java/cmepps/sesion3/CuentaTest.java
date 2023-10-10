990151c72d866df019671f69f4c450267e6bc40a
clopq <clopq@DESKTOP-3DIA8P2>
2023-10-10 16:45:39 +0200

    TDD ejercicio 2 paso , crear prueba

---
+++ src/test/java/cmepps/sesion3/CuentaTest.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package cmepps.sesion3;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author clopq
+ */
+public class CuentaTest {
+    
+    Cuenta ctaPruebas;
+    
+    public CuentaTest() {
+    }
+    
+    @org.junit.jupiter.api.BeforeAll
+    public void setUpClass() throws Exception {
+        ctaPruebas = new Cuenta();
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public void tearDownClass() throws Exception {
+        
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+        
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testIngresar() {
+        ctaPruebas.ingresar(3000);
+        assertEquals(3000,ctaPruebas.getSaldo());
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testRetirar() {
+        ctaPruebas.retirar(3000);
+        assertEquals()
+        
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
